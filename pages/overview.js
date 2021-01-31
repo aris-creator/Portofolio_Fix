407ebdc1a9ea0688c347abe96fdc15d08ffd33e0
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-01-31 05:14:16 +0000

    Update overview.js

--- pages/overview.js
+++ pages/overview.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Overview = () => {
   const overviews = [
-    { content: "Alaraja Food Store",<Link href="https://www.linkedin.com/in/aris-prasetyo-667a2119a">
+    { content: "Alaraja Food Store",<Link> href="https://www.linkedin.com/in/aris-prasetyo-667a2119a">
     </Link>},
   ];
 
